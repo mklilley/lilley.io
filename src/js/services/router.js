efcad00dc936018a66480a923313ccb595d62ec3
Matt Lilley <m.k.lilley@gmail.com>
2020-06-14 08:33:28 +0100

    Changed case of router "Class" to upper case

--- src/js/services/router.js
+++ src/js/services/router.js
@@ -1,7 +1,7 @@
 // Router Adapted from http://krasimirtsonev.com/blog/article/A-modern-JavaScript-router-in-100-lines-history-api-pushState-hash-url
 
 "use strict";
-function router() {
+function Router() {
   return {
     routes: [],
     mode: null,
